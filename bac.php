d47a04092a8e6cb53f84a6f9cc969cbaf3e7203c
FreddyDeveloper <81062362+FreddyDeveloper@users.noreply.github.com>
2023-09-24 16:39:52 -0500

    bac.php (Actualizado Domingo 24 de Septiembre del 2023)

--- bac.php
+++ bac.php
@@ -1,3 +1,10 @@
 clientId = "77700054";
-
 apiKey = "vjtKWhj2sHc3kNW2A3WI1wZVh0nqMsPInSxAhX3Gqp37ACA7XZyUW2";
+
+
+PowerTranz Gateway Api 1.3 ---> https://staging.ptranz.com/api/swagger/index.html
+
+Testear llamadas api ---> https://www.postman.com/dark-space-610460/workspace/fac-payment-gateway/request/14225525-cad29a2c-aaea-4c50-917a-73650b4b5c33
+
+
+
